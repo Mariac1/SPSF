ed7b96f85c54b4a9a60c750c8e6ddfa5b91b117c
Mariac1 <Eugene.Germishuizen@gmail.com>
2015-10-09 02:00:43 +0200

    Calendar
    Fixed the Calendar exit and re-enter

--- app/src/main/java/superponystrikeforce/remember/ListViewActivity.java
+++ app/src/main/java/superponystrikeforce/remember/ListViewActivity.java
@@ -1,6 +1,14 @@
 package superponystrikeforce.remember;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -15,11 +23,14 @@ public class ListViewActivity extends Activity implements OnClickListener {
     private ListView lv_android;
     private AndroidListAdapter list_adapter;
     private Button btn_calender;
+    //File file = new File("CalendarList.txt");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+        //getList();
         getWidget();
         Button b = (Button) findViewById(R.id.btn_addEvent);
         b.setOnClickListener(new OnClickListener() {
@@ -30,6 +41,50 @@ public class ListViewActivity extends Activity implements OnClickListener {
         });
     }
 
+    private void getList() {
+        /*if (file.exists()) {
+            String fileName = "CalendarList.txt";
+            try {
+                String message;
+                FileInputStream fileInputStream = openFileInput(fileName);
+                InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                StringBuffer stringBuffer = new StringBuffer();
+                while ((message = bufferedReader.readLine()) != null) {
+                    stringBuffer.append(message).append("\n");
+                }
+
+                //String hantering
+                StringTokenizer tokens = new StringTokenizer(stringBuffer.toString(), "\n");
+                ArrayList<String> name = new ArrayList<>();
+                ArrayList<String> date = new ArrayList<>();
+                while (tokens.nextToken() != "") {
+                    date.add(tokens.nextToken());
+                    name.add(tokens.nextToken());
+                }
+                System.out.println(date);
+                System.out.println(name);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            String fileName = "CalendarList.txt";
+            try {
+                //write to the new diary file
+                FileOutputStream fileOutputStream = openFileOutput(fileName, MODE_PRIVATE);
+                fileOutputStream.write(" ".getBytes());
+                fileOutputStream.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }*/
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == 2) {
@@ -37,6 +92,7 @@ public class ListViewActivity extends Activity implements OnClickListener {
             String EventDate = bundle.getString("date");
             String EventName = bundle.getString("name");
             CalendarCollection.date_collection_arr.add(new CalendarCollection(EventDate, EventName));
+            //writeToCalendar(EventDate, EventName);
             getWidget();
         }
     }
@@ -47,8 +103,23 @@ public class ListViewActivity extends Activity implements OnClickListener {
         btn_calender.setOnClickListener(this);
 
         lv_android = (ListView) findViewById(R.id.lv_android);
-        list_adapter = new AndroidListAdapter(ListViewActivity.this, R.layout.list_item, CalendarCollection.date_collection_arr);
         lv_android.setAdapter(list_adapter);
+        list_adapter = new AndroidListAdapter(ListViewActivity.this, R.layout.list_item, CalendarCollection.date_collection_arr);
+    }
+
+    private void writeToCalendar(String Date, String Name) {
+        String fileName = "CalendarList.txt";
+        String string = Date + "\n" + Name;
+        FileOutputStream fileOutputStream;
+        try {
+            fileOutputStream = openFileOutput(fileName, MODE_PRIVATE);
+            fileOutputStream.write(string.getBytes());
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
--- app/src/main/java/superponystrikeforce/remember/MenuActivity.java
+++ app/src/main/java/superponystrikeforce/remember/MenuActivity.java
@@ -1,6 +1,8 @@
 package superponystrikeforce.remember;
 
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,9 +13,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 
 public class MenuActivity extends AppCompatActivity {
@@ -23,19 +32,11 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        createDiaryNrFile();
+//      createDiaryNrFile();
+//      createCalendar();
 
         Button Calendar_BTN = (Button) findViewById(R.id.Calendar);
         Calendar_BTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), CalenderActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button Reminder_BTN = (Button) findViewById(R.id.Reminder);
-        Reminder_BTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), ListViewActivity.class);
@@ -43,6 +44,15 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
+       /* Button Reminder_BTN = (Button) findViewById(R.id.Reminder);
+        Reminder_BTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ListViewActivity.class);
+                startActivity(intent);
+            }
+        });
+*/
         Button Diary_BTN = (Button) findViewById(R.id.Diary);
         Diary_BTN.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +70,36 @@ public class MenuActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void createCalendar() {
+        /*String fileName = "CalendarList.txt";
+        File file = new File(fileName);
+        if (!file.exists()){
+            String string = "";
+            FileOutputStream outputStream;
+
+            try {
+                outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+                outputStream.write(string.getBytes());
+                outputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }*/
+       /* String fileName = "CalendarList.txt";
+        String Empty = "";
+        try {
+            //write to the new diary file
+            FileOutputStream fileOutputStream = openFileOutput(fileName, MODE_PRIVATE);
+            fileOutputStream.write(Empty.getBytes());
+            fileOutputStream.close();
+            Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
     }
 
     public void createDiaryNrFile(){
